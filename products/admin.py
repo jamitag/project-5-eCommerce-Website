0cd2285f6163d7bf96fcfab8e8d2f7ebc80566f0
jamitag <jamgra12@gmail.com>
2022-07-15 10:22:27 +0000

    Updated admin list

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from .models import Product, Category
 
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+    )
+
+    ordering = ('sku',)
+
 # Register your models here.
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
 admin.site.register(Category)
