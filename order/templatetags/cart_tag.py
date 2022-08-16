e480f176da9e684fed2c4ffbca7279157a6a38db
jamitag <jamgra12@gmail.com>
2022-08-16 10:43:58 +0000

    Functionality to show number of cart items in navbar

---
+++ order/templatetags/cart_tag.py
@@ -0,0 +1,14 @@
+from atexit import register
+from django import template
+from order.models import Order
+
+register = template.Library()
+
+@register.filter
+
+def cart_total(user):
+    order = Order.objects.filter(user=user, ordered=False)
+    if order.exists():
+        return order[0].orderItems.count()
+    else:
+        return 0
