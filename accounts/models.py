80f306a2072840f62be92c3d9059ff5cfebffcad
jamitag <jamgra12@gmail.com>
2022-08-19 10:22:31 +0000

    Create additional checkout functionality - total items, save address

--- accounts/models.py
+++ accounts/models.py
@@ -4,9 +4,18 @@ from django.contrib.auth.models import User
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     avatar = models.ImageField(default='default.png', upload_to='profile_images')
     bio = models.TextField()
 
     def __str__(self):
         return self.user.username
+
+    def is_fully_filled(self):
+        fields_names = [f.name for f in self._meta.get_fields()]
+        for field_name in fields_names:
+            value = getattr(self, field_name)
+            if value is None or value =='':
+                return False
+        return True
+
--- checkout/views.py
+++ checkout/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponseRedirect
+from django.shortcuts import render, HttpResponseRedirect, redirect
 from order.models import Order
 from .models import BillingAddress
 from .forms import BillingAddressForm
@@ -13,6 +13,8 @@ def checkout(request):
     saved_address = BillingAddress.objects.get_or_create(user=request.user)
     saved_address = saved_address[0]
 
+
+# Save address for future use
     form = BillingAddressForm(instance=saved_address)
     if request.method == 'POST':
         form = BillingAddressForm(request.POST, instance=saved_address)
@@ -20,5 +22,23 @@ def checkout(request):
             form.save()
             form = BillingAddressForm(instance=saved_address)
             messages.success(request, f'Delivery Address Saved')
-    return render(request, 'checkout/checkout.html', context={'form':form})
+    
+    order_qs = Order.objects.filter(user=request.user, ordered=False)
+    order_items = order_qs[0].orderItems.all()
+    order_total = order_qs[0].get_totals()
+    return render(request, 'checkout/checkout.html', context={'form':form, 'order_items':order_items, 'order_total':order_total, 'saved_address':saved_address})
+
+
+@login_required
+def payment(request):
+    saved_address = BillingAddress.objects.get_or_create(user=request.user)
+    if not saved_address[0].is_fully_filled():
+        messages.info(request, 'Please complete delivery address')
+        return redirect('checkout')
+
+    if not request.user.profile.is_fully_filled():
+        messages.info(request, 'Please complete profile')
+        return redirect('users-profile')
+    return render(request, 'checkout/payment.html', context={})
+
 
